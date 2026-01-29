966d1893cce06d2a6f0af0a5103cbb6b50f0f40c
bpmryan <bpm_ryand2428@yahoo.com>
2026-01-29 14:38:40 -0600

    Add basic js stucture for library

--- code/js/script.js
+++ code/js/script.js
@@ -1,29 +1,26 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
-    // constructor
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the contructor");
-    }
-    /**
-     * Contains information about book:
-     * title
-     * author
-     * pages
-     */
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-
-    // not sure if needed (test)
-    this.listBook = function() {
-        console.log(this.title)
-    };
+// constructors
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the contructor");
+  }
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-    // take params
-    // create a book then store it in the array
-    // id createBook
+Book.prototype.listBook = function () {
+  this.read = !this.read;
+};
 
+function addBookToLibrary() {
+  // take params
+  // create a book then store it in the array
+  // id createBook
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  displayBooks();
 }
