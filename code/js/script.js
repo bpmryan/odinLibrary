81ff5009a2a0cfbf735205b6ae7dff102c7ff93d
bpmryan <bpm_ryand2428@yahoo.com>
2026-02-02 21:00:47 -0600

    Create functions to add books and display them, not finished

--- code/js/script.js
+++ code/js/script.js
@@ -16,13 +16,41 @@ Book.prototype.toggleRead = function () {
   this.read = !this.read;
 };
 
+/**
+ * take params
+ * create a book then store it in the array
+ * id createBook
+ */
 function addBookToLibrary() {
-  // take params
-  // create a book then store it in the array
-  // id createBook
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
   displayBooks();
+}
+
+const shelf = document.getElementById("shelf");
+
+/**
+ * Pipes in HTML for each book put into array
+ * creates a div for each book in the array
+ */
+function displayBooks() {
+  shelf.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("book", book.read ? "read" : "unread"); // reads form to see if the book has been read yet
+    div.dataset.bookId = book.id;
+
+    const h2 = document.createElement("h2");
+    h2.textContent = book.title;
+
+    const h3 = document.createElement("h3");
+    h3.textContent = book.author;
+
+    const p = document.createElement("p");
+    p.textContent = `${book.pages} pages`;
+
+    
+  })
 }
 
 form.addEventListener('submit', (e) => {
